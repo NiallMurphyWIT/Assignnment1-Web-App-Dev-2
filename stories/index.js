2a2ff9ca1604454f7893b481e0c46046ea35a5fd
Niall Murphy <niallmurphywit@gmail.com>
2019-11-11 14:46:23 +0000

    Comment page stories

--- stories/index.js
+++ stories/index.js
@@ -5,7 +5,9 @@ import PostForm from '../src/components/postForm/';
 import PostItem from '../src/components/postItem';
 import PostList from '../src/components/postList';
 import { action } from "@storybook/addon-actions";
-
+import CommentForm from "../src/components/commentForm";
+import Comment from "../src/components/comment";
+import CommentList from "../src/components/commentList";
 
 const post = {
     id: 1 ,
@@ -34,3 +36,31 @@ storiesOf('Game App/Post Item', module)
         ];
     return <PostList posts= { defaultPosts }  />
 });
+
+storiesOf("Game App/Comment page/Comment Form", module).add("default", () => (
+    <CommentForm post={post} addCommentHandler={action("comment added")} />
+  ));
+
+  const comment = {
+    id: 1,
+    author: "Joe Bloggs",
+    comment: "Very informative .....",
+    upvotes: 10
+  };
+
+
+  storiesOf("Game App/Comment page/Comment", module).add("default", () => (
+    <Comment upvoteHandler={action("upvoted")} comment={comment} />
+  ));
+  
+  storiesOf("Game App/Comment page/Comment list", module).add("default", () => {
+    const defaultComments = [
+      comment,
+      { ...comment, author: "Jane Smith", upvotes: 3, id: 2 },
+      { ...comment, comment: "On the other hand", id: 3 },
+      { ...comment, author: "Jill Dwyer", upvotes: 5, id: 4 }
+    ];
+    return (
+      <CommentList upvoteHandler={action("upvoted")} comments={defaultComments} />
+    );
+  });
