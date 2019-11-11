650b7282fd428c6c7793201acae7d92f07fa0dcb
Niall Murphy <niallmurphywit@gmail.com>
2019-11-11 13:27:57 +0000

    PostForm component completed

---
+++ src/components/postForm/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './postForm.css';
+
+export default class Form extends Component {
+    render() {
+        return (
+        <form  className="form bg-dark text-light">
+            <h3>Add a game news post</h3>
+            <div className="form-group">
+              <input type="text"
+                className="form-control"
+                placeholder="Post Title"></input>
+            </div>
+            <div className="form-group">
+              <input type="text"
+                className="form-control"
+                placeholder="Name of Author"></input>
+            </div>
+            <div className="form-group">
+                <input type="text"
+                  className="form-control"
+                placeholder="Link to Article etc."></input>
+            </div>
+            <button type="submit" className="btn btn-primary">Add</button>
+        </form>
+        );
+    }
+}
--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import PostForm from '../src/components/postForm/';
+
+
+storiesOf("Game App/Post Form", module).add("default", () => <PostForm />);
