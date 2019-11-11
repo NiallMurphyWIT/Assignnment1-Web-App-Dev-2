998d218d31b703703c68b2103166eae139a8f33c
Niall Murphy <niallmurphywit@gmail.com>
2019-11-11 13:35:23 +0000

    PostItem component completed

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import PostForm from '../src/components/postForm/';
+import PostItem from '../src/components/postItem';
+
+
+const post = {
+    id: 1 ,
+    title : 'Post 1. Patch Notes',
+    link : 'https://tftactics.gg/patch-notes',
+    author : 'Joey',
+    comments : [],
+    upvotes: 10
+};
 
 
 storiesOf("Game App/Post Form", module).add("default", () => <PostForm />);
+
+
+storiesOf('Game App/Post Item', module)
+    .add('default', () => <PostItem post= { post }  />
+    )
+    .add('No hyperlink', () => <PostItem post= { { ...post, link: ''} }  />
+    )
